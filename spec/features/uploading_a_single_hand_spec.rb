e18f230ef14f26c905701c88c2e5b027050abdb6
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-04-14 10:28:39 +0100

    refactor upload single hand spec to include before hook

--- spec/features/uploading_a_single_hand_spec.rb
+++ spec/features/uploading_a_single_hand_spec.rb
@@ -1,11 +1,13 @@
 describe "uploading a single hand" do
-  it 'uploads without error' do
+  before(:all) do
     visit '/hand_histories/new'
 
     attach_file("hand_history_file",Rails.root + "files for testing/1 cash hand example.txt")
 
     click_button
+  end
 
+  it 'uploads without error' do
     expect(page).to have_content("hand 1")
   end
 end
