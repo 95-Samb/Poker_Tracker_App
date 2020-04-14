29624f3848e09a75555561c9d87670dc2410a54d
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-04-14 10:40:33 +0100

    initial dummy testing for single hand spec

--- spec/features/uploading_a_single_hand_spec.rb
+++ spec/features/uploading_a_single_hand_spec.rb
@@ -1,5 +1,5 @@
 describe "uploading a single hand" do
-  before(:all) do
+  before(:each) do
     visit '/hand_histories/new'
 
     attach_file("hand_history_file",Rails.root + "files for testing/1 cash hand example.txt")
@@ -9,5 +9,14 @@ describe "uploading a single hand" do
 
   it 'uploads without error' do
     expect(page).to have_content("hand 1")
+  end
+
+  describe "Relevant information displayed" do
+    it "displays Hero's holding" do
+      expect(page).to have_content("Hero: 7c,Ac")
+    end
+    it "displays Hero's preflop action" do
+      expect(page).to have_content("Hero folds")
+    end
   end
 end
