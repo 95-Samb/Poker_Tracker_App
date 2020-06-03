c0b66a7950fae7ac27a451977f2e778e4296db42
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-06-03 12:15:36 +0100

    method added to found amount of money won/lost in hand

--- app/models/hand_history.rb
+++ app/models/hand_history.rb
@@ -6,7 +6,24 @@ class HandHistory < ApplicationRecord
   end
 
   def self.money_won(input)
-    -0.01
+    user = HandHistory.user(input) + ":"
+    money = 0
+    input.each_line do |line|
+      if line.include?(user)
+        money -= line.split(" ").last[1..-1].to_f
+      elsif line.include?(user[0..-2] + " collected")
+        money = line.split(" ")[2][1..-1].to_f
+      end
+    end
+    money
+  end
+
+  def self.user(input)
+    input.each_line do |line|
+      if line[0..4] == "Dealt"
+        return line.split(" ")[2]
+      end
+    end
   end
 
 end
--- spec/models/hand_history_spec.rb
+++ spec/models/hand_history_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe HandHistory, type: :model do
     it "returns money won" do
       expect(HandHistory.money_won(@hand_history)).to eq(-0.01)
     end
+    it "identifies the correct user" do
+      expect(HandHistory.user(@hand_history)).to eq("StuffedPeppr")
+    end
   end
   context "for 2nd 1 cash hand example" do
     before(:all) do
@@ -21,8 +24,17 @@ RSpec.describe HandHistory, type: :model do
     it "returns the correct number" do
       expect(HandHistory.hand_number(@hand_history)).to eq(208415618147)
     end
-    xit "returns money won" do
+    it "returns money won" do
       expect(HandHistory.money_won(@hand_history)).to eq(0)
+    end
+  end
+  context "for 3rd 1 cash hand example" do
+    before(:all) do
+      file = File.open(Rails.root.join "files for testing/3rd 1 cash hand example.txt")
+      @hand_history = file.read
+    end
+    it "returns money won" do
+      expect(HandHistory.money_won(@hand_history)).to eq(0.52)
     end
   end
 end
